7277b8334ce6dc1e26d02f4dd4bca77802bff572
BojanDolic <dolicbojan2@gmail.com>
2022-05-13 10:46:03 +0200

    DEVELOP_1305_FLAT_PRIMARY
    Add flat coil image
    Remove form widget in flat_coil_screen.dart
    Add Appbar widget in flat_coil_screen.dart
    Add function for flat spiral dialog in DialogUtil.dart

--- lib/dialogs/DialogUtil.dart
+++ lib/dialogs/DialogUtil.dart
@@ -50,4 +50,52 @@ class DialogUtil {
       },
     );
   }
+
+  static void openFlatSpiralCoilInfoDialog(BuildContext context) {
+    final theme = Theme.of(context);
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          backgroundColor: theme.dialogTheme.backgroundColor,
+          shape: roundedBorder16,
+          content: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(
+                  height: 12,
+                ),
+                Image.asset(
+                  "assets/formulas/helical_coil_formula_200.png",
+                  color: theme.dialogTheme.contentTextStyle?.color,
+                ),
+                const SizedBox(
+                  height: 24,
+                ),
+                Text(
+                  "Formulas used in this calculator are formulas derived by Harold A. Wheeler.\n"
+                  "He proposed formula shown above to be used for determining inductance "
+                  "of finite-length solenoid made of round wire."
+                  "\nThe values entered must be in inches."
+                  "\nResultant inductance is in microhenries (μH)"
+                  "\n\nReference: Harold A. Wheeler, \"Simple Inductance Formulas for Radio Coils,\" Proceedings of the I.R.E., October 1928, pp. 1398-1400",
+                  style: theme.dialogTheme.contentTextStyle,
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Close"),
+            ),
+          ],
+          actionsAlignment: MainAxisAlignment.center,
+        );
+      },
+    );
+  }
 }
--- lib/screens/calculators/flat_coil_screen.dart
+++ lib/screens/calculators/flat_coil_screen.dart
@@ -1,3 +1,4 @@
+import 'package:coiler_app/dialogs/DialogUtil.dart';
 import 'package:coiler_app/providers/FlatCoilProvider.dart';
 import 'package:coiler_app/util/constants.dart';
 import 'package:coiler_app/util/extensions/theme_extension.dart';
@@ -31,124 +32,146 @@ class _FlatCoilScreenState extends State<FlatCoilScreen> {
     final provider = Provider.of<FlatCoilProvider>(context);
     return Scaffold(
       backgroundColor: theme.backgroundColor,
+      appBar: AppBar(
+        elevation: 1,
+        centerTitle: true,
+        title: Text(
+          "Flat coil",
+          style: theme.textTheme.headlineMedium,
+        ),
+        actions: [
+          PopupMenuButton<String>(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(16),
+            ),
+            itemBuilder: (context) {
+              return popupCalculatorScreenInfo;
+            },
+            onSelected: (value) {
+              if (value == actionInformation) {
+                DialogUtil.openFlatSpiralCoilInfoDialog(context);
+              }
+            },
+          ),
+        ],
+      ),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(9.0),
-            child: Form(
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  // TODO Add flat spiral coil image
-                  const CalculatorDescription(
-                    imagePath: "assets/helical_coil_transparent.png",
-                    description: "Calculate inductance of flat spiral coil using this calculator. Result is calculated automatically.",
-                  ),
-                  const SizedBox(
-                    height: 9,
-                  ),
-                  BorderContainer(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Flexible(
-                          child: Text(
-                            "Inductance: ${provider.inductance}",
-                            maxLines: 2,
-                            overflow: TextOverflow.ellipsis,
-                            style: theme.textTheme.displayMedium,
-                          ),
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                // TODO Add flat spiral coil image
+                const CalculatorDescription(
+                  imagePath: "assets/flat_spiral_coil.png",
+                  description: "Calculate inductance of flat spiral coil using this calculator. Result is calculated automatically.",
+                  width: double.infinity,
+                  height: 130,
+                ),
+                const SizedBox(
+                  height: 9,
+                ),
+                BorderContainer(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        child: Text(
+                          "Inductance: ${provider.inductance}",
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: theme.textTheme.displayMedium,
                         ),
-                        DropDownMenu<Units>(
-                          value: provider.inductanceUnit,
-                          items: inductanceDropDownList,
-                          onSelect: (unit) {
-                            provider.setInductanceUnit(unit!);
-                            provider.calculateInductance();
-                          },
-                        ),
-                      ],
-                    ),
+                      ),
+                      DropDownMenu<Units>(
+                        value: provider.inductanceUnit,
+                        items: inductanceDropDownList,
+                        onSelect: (unit) {
+                          provider.setInductanceUnit(unit!);
+                          provider.calculateInductance();
+                        },
+                      ),
+                    ],
                   ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  InputFieldDropDown(
-                    controller: innerDiameterTextController,
-                    hintText: "Inner diameter of the coil",
-                    labelText: "Inner diameter",
-                    onTextChanged: (newText) {
-                      provider.validateInnerDiameter(double.tryParse(newText));
-                      provider.calculateInductance();
-                    },
-                    validator: (value) => null,
-                    errorText: provider.innerDiameter.error,
-                    dropDownValue: provider.innerDiameterUnit,
-                    onDropDownChanged: (value) {
-                      provider.setInnerDiameterUnit(value!);
-                      provider.calculateInductance();
-                    },
-                    dropDownList: lengthDropDownList,
-                  ),
-                  const SizedBox(
-                    height: 9,
-                  ),
-                  InputFieldDropDown(
-                    controller: wireDiameterTextController,
-                    hintText: "Wire diameter of the coil",
-                    labelText: "Wire diameter",
-                    onTextChanged: (newText) {
-                      provider.validateWireDiameter(double.tryParse(newText));
-                      provider.calculateInductance();
-                    },
-                    validator: (value) => null,
-                    errorText: provider.wireDiameter.error,
-                    dropDownValue: provider.wireDiameterUnit,
-                    onDropDownChanged: (value) {
-                      provider.setWireDiameterUnit(value!);
-                      provider.calculateInductance();
-                    },
-                    dropDownList: lengthDropDownList,
-                  ),
-                  const SizedBox(
-                    height: 9,
-                  ),
-                  InputFieldDropDown(
-                    controller: turnSpacingTextController,
-                    hintText: "Turn spacing of the coil",
-                    labelText: "Turn spacing",
-                    onTextChanged: (newText) {
-                      provider.validateTurnSpacing(double.tryParse(newText));
-                      provider.calculateInductance();
-                    },
-                    validator: (value) => null,
-                    errorText: provider.turnSpacing.error,
-                    dropDownValue: provider.wireSpacingUnit,
-                    onDropDownChanged: (value) {
-                      provider.setTurnSpacingUnit(value!);
-                      provider.calculateInductance();
-                    },
-                    dropDownList: lengthDropDownList,
-                  ),
-                  const SizedBox(
-                    height: 9,
-                  ),
-                  InputField(
-                    controller: turnsTextController,
-                    onTextChanged: (text) {
-                      provider.validateTurns(int.tryParse(text));
-                      provider.calculateInductance();
-                    },
-                    validator: (text) => null,
-                    hintText: "Enter number of turns",
-                    labelText: "Turns",
-                    errorText: provider.turns.error,
-                    unitText: "No",
-                    inputFormatter: [FilteringTextInputFormatter.digitsOnly],
-                  ),
-                ],
-              ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                InputFieldDropDown(
+                  controller: innerDiameterTextController,
+                  hintText: "Inner diameter of the coil",
+                  labelText: "Inner diameter (Di)",
+                  onTextChanged: (newText) {
+                    provider.validateInnerDiameter(double.tryParse(newText));
+                    provider.calculateInductance();
+                  },
+                  validator: (value) => null,
+                  errorText: provider.innerDiameter.error,
+                  dropDownValue: provider.innerDiameterUnit,
+                  onDropDownChanged: (value) {
+                    provider.setInnerDiameterUnit(value!);
+                    provider.calculateInductance();
+                  },
+                  dropDownList: lengthDropDownList,
+                ),
+                const SizedBox(
+                  height: 9,
+                ),
+                InputFieldDropDown(
+                  controller: wireDiameterTextController,
+                  hintText: "Wire diameter of the coil",
+                  labelText: "Wire diameter (W)",
+                  onTextChanged: (newText) {
+                    provider.validateWireDiameter(double.tryParse(newText));
+                    provider.calculateInductance();
+                  },
+                  validator: (value) => null,
+                  errorText: provider.wireDiameter.error,
+                  dropDownValue: provider.wireDiameterUnit,
+                  onDropDownChanged: (value) {
+                    provider.setWireDiameterUnit(value!);
+                    provider.calculateInductance();
+                  },
+                  dropDownList: lengthDropDownList,
+                ),
+                const SizedBox(
+                  height: 9,
+                ),
+                InputFieldDropDown(
+                  controller: turnSpacingTextController,
+                  hintText: "Turn spacing of the coil",
+                  labelText: "Turn spacing (S)",
+                  onTextChanged: (newText) {
+                    provider.validateTurnSpacing(double.tryParse(newText));
+                    provider.calculateInductance();
+                  },
+                  validator: (value) => null,
+                  errorText: provider.turnSpacing.error,
+                  dropDownValue: provider.wireSpacingUnit,
+                  onDropDownChanged: (value) {
+                    provider.setTurnSpacingUnit(value!);
+                    provider.calculateInductance();
+                  },
+                  dropDownList: lengthDropDownList,
+                ),
+                const SizedBox(
+                  height: 9,
+                ),
+                InputField(
+                  controller: turnsTextController,
+                  onTextChanged: (text) {
+                    provider.validateTurns(int.tryParse(text));
+                    provider.calculateInductance();
+                  },
+                  validator: (text) => null,
+                  hintText: "Enter number of turns",
+                  labelText: "Turns",
+                  errorText: provider.turns.error,
+                  unitText: "No",
+                  inputFormatter: [FilteringTextInputFormatter.digitsOnly],
+                ),
+              ],
             ),
           ),
         ),
@@ -162,10 +185,14 @@ class CalculatorDescription extends StatelessWidget {
     Key? key,
     required this.imagePath,
     required this.description,
+    this.width,
+    this.height,
   }) : super(key: key);
 
   final String imagePath;
   final String description;
+  final double? width;
+  final double? height;
 
   @override
   Widget build(BuildContext context) {
@@ -178,6 +205,9 @@ class CalculatorDescription extends StatelessWidget {
           Image.asset(
             imagePath,
             color: isDark ? Colors.grey.shade400 : Colors.black,
+            width: (width != null) ? width : 150,
+            height: (height != null) ? height : 250,
+            fit: BoxFit.fill,
           ), //"assets/helical_coil_transparent.png"
           Text(
             description,
